64f1acd05790a1602b3f6fbf84f2ec766417dfff
jc <46619361+juancwu@users.noreply.github.com>
2023-10-12 23:17:35 -0400

    add quote module

--- hello.go
+++ hello.go
@@ -2,7 +2,12 @@ package main
 
 import "fmt" // packaeg for printing to stdio
 
+import "rsc.io/quote"
+
 // entry point
 func main() {
     fmt.Println("Gorangu!")
+
+    fmt.Println("Quote:")
+    fmt.Println(quote.Go())
 }
