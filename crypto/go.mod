e3aa43a14b39cdda966fa8adb2968994a75b0db2
jc <46619361+juancwu@users.noreply.github.com>
2023-10-30 23:40:10 -0400

    playing with crypto in golang

---
+++ crypto/go.mod
@@ -0,0 +1,3 @@
+module github.com/juancwu/Gorangu/crypto
+
+go 1.21.3
---
+++ crypto/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "crypto/aes"
+    "crypto/cipher"
+    "crypto/rand"
+)
+
+func main() {
+
+}
+
+// func encrypt(key []byte, data []byte) ([]byte, error) {
+// }
